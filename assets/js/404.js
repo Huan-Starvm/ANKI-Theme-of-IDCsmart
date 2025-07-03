75efe76d9494b99381254cf995483c97c45a98e0
欢 <2711452309@qq.com>
2025-07-03 13:23:16 +0800

    Update 404.js
    Signed-off-by: 欢 <2711452309@qq.com>

--- assets/js/404.js
+++ assets/js/404.js
@@ -1,6 +1,6 @@
-document.addEventListener("DOMContentLoaded", () => {
+document。addEventListener("DOMContentLoaded", () => {
   // Set page title
-  document.title = "404 页面未找到 | AnkiCloud"
+  document.title = "404 页面未找到 | {$setting.company_name}"
 
   // Add any additional functionality for the 404 page here
 })
